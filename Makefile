46b8572aa5b4b15b3df4826281acf4c483993df1
Peter Waller <p@pwaller.net>
2015-04-17 23:32:30 +0100

    First working implementation

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
-build: .FORCE
+associate-eip: .FORCE
 	docker build -t associate-eip .
+	docker run --rm associate-eip cat /go/bin/associate-eip > associate-eip
+	chmod u+x associate-eip
 
 .PHONY: .FORCE
 .FORCE:
--- main.go
+++ main.go
@@ -1,25 +1,91 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/awslabs/aws-sdk-go/aws"
-	"github.com/awslabs/aws-sdk-go/aws/awsutil"
 	"github.com/awslabs/aws-sdk-go/service/ec2"
 )
 
-func main() {
+func Metadata(path string) (string, error) {
+	resp, err := http.Get("http://169.254.169.254/latest/meta-data/" + path)
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
 
-	svc := ec2.New(nil)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func ThisInstanceID() (string, error) {
+	return Metadata("instance-id")
+}
+
+func ThisAvailabilityZone() (string, error) {
+	result, err := Metadata("placement/availability-zone")
+	if err == nil {
+		result = result[:len(result)-1]
+	}
+	return result, err
+}
+
+func main() {
+	log.SetFlags(0)
+
+	args := os.Args[1:]
+	if len(args) < 1 {
+		log.Fatal("usage: %s <PublicIP>", os.Args[0])
+	}
+	publicIP := args[0]
+
+	thisInstanceID, err := ThisInstanceID()
+	if err != nil {
+		log.Fatalf("Unable to determine instance id: %v", err)
+	}
+
+	thisAvailabilityZone, err := ThisAvailabilityZone()
+	if err != nil {
+		log.Fatalf("Unable to determine availability zone: %v", err)
+	}
+
+	log.Println("InstanceID:", thisInstanceID, "AZ:", thisAvailabilityZone)
+
+	svc := ec2.New(&aws.Config{
+		Region: thisAvailabilityZone,
+	})
 
 	desc, err := svc.DescribeAddresses(&ec2.DescribeAddressesInput{
-		PublicIPs: []*string{aws.String("52.16.160.41")},
+		PublicIPs: []*string{aws.String(publicIP)},
 	})
 
 	if err != nil {
-		panic(err)
+		log.Fatalf("Unable to describe EIPs: %v", err)
 	}
 
-	fmt.Println(awsutil.StringValue(desc.Addresses))
-	// svc.AssociateAddress(&ec2.AssociateAddressInput{})
+	if len(desc.Addresses) != 1 {
+		log.Fatalf("Expected exactly 1 address, got %v", len(desc.Addresses))
+	}
+
+	allocation := desc.Addresses[0]
+
+	resp, err := svc.AssociateAddress(&ec2.AssociateAddressInput{
+		InstanceID:         &thisInstanceID,
+		AllowReassociation: aws.Boolean(true),
+		AllocationID:       allocation.AllocationID,
+	})
+
+	if err != nil {
+		log.Fatalf("Unable to associate allocation: %v", err)
+	}
+
+	log.Println("Associated:", *resp.AssociationID)
 }
